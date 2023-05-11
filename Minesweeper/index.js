e3e946cfd13e9dfa7f91c28cb5a6e48b22b65221
varenik40000 <64190191+varenik40000@users.noreply.github.com>
2023-05-11 20:28:30 +0500

    feat: add render page

---
+++ Minesweeper/index.js
@@ -0,0 +1,31 @@
+function render() {
+    let body = document.body;
+    return body.innerHTML = `
+    <div class="header">
+     <div class="container">
+       <h1 class="header-title">Minesweeper</h1>
+     </div>
+    </div>
+    <main class="main">
+      <div class="container">
+        <h1 class="main-title">Mines: <span id="mines-count">0</span></h1>
+        <div id="board"></div>
+      </div>
+    </main>
+    <footer class="footer">
+     <div class="container">
+         <div class="footer__inner">
+             <div class="footer_github">© 2022 github</div>
+             <div class="footer_rss">Rolling Scopes School</div>
+             <div class="footer__contact">
+                 <a href="#" class="footer__contact-telegram"></a>
+                 <a href="#" class="footer__contact-discord"></a>
+             </div>
+         </div>
+     </div>
+    </footer>
+        `
+}
+
+render()
+
